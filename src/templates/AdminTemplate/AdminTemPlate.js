8514f7068f62fd12ae8dc7311df33d3c54111c0a
thiensinh127 <thiensinh127@gmail.com>
2021-07-07 17:25:23 +0700

    sinhUpd

--- src/templates/AdminTemplate/AdminTemPlate.js
+++ src/templates/AdminTemplate/AdminTemPlate.js
@@ -1,4 +1,4 @@
-import React, { Fragment } from "react";
+import React from "react";
 import { Redirect, Route } from "react-router-dom";
 
 import { USER_LOGIN } from "../../redux/constants/NguoiDungConst";
